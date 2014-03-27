2868305be3d1ba4ab419e047ff99980d99dbeb1c
williewonka <frankieboy03@gmail.com>
2014-03-27 11:41:33 +0100

    added toolbox analyses

---
+++ toolbox_analysis.py
@@ -0,0 +1,57 @@
+__author__ = 'williewonka'
+
+from toolset_mapping import ToolBox
+import openpyxl
+from json import dumps
+import sys
+
+#globals
+toolboxes = []
+results = {} #dictionary of results, key is patent, value is dictionary with toolbox as key and weight as value
+toolboxesresult = {} #dictionary of toolboxes, key is the number of toolbox, value is dictionary with key patent and value weight
+#load the patentdata
+wb = openpyxl.load_workbook("patentdata/patentdata.xlsx")
+sheet = wb.get_active_sheet()
+abstracts = sheet.columns[1]
+
+#load the toolboxes encoding
+stream = open("toolboxes.tb","r")
+# toolboxfile = stream.readlines()
+#create the seperate toolbox classes and store them in global
+i = 0
+for line in stream:
+    line = line.strip("\n")
+    try:
+        toolboxes.append(ToolBox(line))
+    except:
+        sys.exit("Parse error at line " + str(i))
+    i += 1
+
+
+#iterate through the abstracts, remember the index number
+i = 1 #skip first row
+while i < len(abstracts):
+    j = 0
+    results[i] = {}
+    while j < len(toolboxes):
+        result = toolboxes[j].Check(abstracts[i].value.split(" "))
+        if result == 0:
+            j += 1
+            continue
+        try:
+            toolboxesresult[j][i] = result
+        except:
+            toolboxesresult[j] = {}
+            toolboxesresult[j][i] = result
+        results[i][j] = result
+        j += 1
+    i += 1
+
+
+#dump results
+stream = open("json/toolboxresult_patents.json","w")
+stream.writelines(dumps(results))
+stream.close()
+stream = open("json/toolboxresult_toolboxes.json","w")
+stream.writelines(dumps(toolboxesresult))
+stream.close()
---
+++ toolset_mapping.py
@@ -0,0 +1,69 @@
+__author__ = 'williewonka'
+
+class ToolBox:
+    sentenceweight = 5
+    searchwidth = 5
+    def __init__(self, encoding):
+        #constructor, reads and stores the encoding of the toolset in a list of dictionaries
+        #every item in list represents a sentence
+        #the sentence itself is a dictionary with words as keys and weight as values
+        self.sentences = []
+        for line in encoding.split("%"):#split on the sentences
+            sentence = {}
+            for word in line.split(" "):
+                if word == "":#skip empty entries cause by double spacing
+                    continue
+                sentence[word.split('|')[0]] = int(word.split('|')[1]) #save the word and weight
+            if sentence:
+                self.sentences.append(sentence) #save the sentence
+
+    def Check(self, line):
+        #checks the line against the encoding (line already split)
+        #looks also around the words to check for sentences
+        #returns the weight for the specific toolbox
+        #check word for word and crossreference to the encoding
+        #the total weight
+        weight = 0
+        #keep track of index so that jumping is possible
+        i = 0
+        while i < len(line):
+            word = line[i]
+            #iterate through the sentences and check the word
+            for sentence in self.sentences:
+                #iterate through all the words
+                temp_weight = self.CheckSentence(word,sentence)
+                #if the word is hit in this sentence, look around to check for other parts of sentence
+                if temp_weight > 0:
+                    #clip the search line to keep within the line
+                    z = i - self.searchwidth
+                    if z < 0:
+                        z = 0
+                    end = i + self.searchwidth
+                    if end > len(line):
+                        end = len(line)
+                    while z < end:
+                        if i == z:
+                            z += 1
+                            continue
+                        result = self.CheckSentence(line[z],sentence)
+                        if result > 0:
+
+                            #apply sentence weight
+                            if result > temp_weight:
+                                temp_weight = result
+                            temp_weight *= self.sentenceweight
+                            break
+                        z += 1
+                #ready with sentence checking, apply the weight
+                weight += temp_weight
+            i += 1
+        return weight
+
+
+    def CheckSentence(self, word, sentence): #checks the sentence, returns weight
+        if word == "":
+            return 0
+        for keyword in list(sentence.keys()):
+            if keyword.lower() in word.lower():
+                return sentence[keyword]
+        return 0
